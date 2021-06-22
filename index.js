c7628ccbd5a82d9d1601a63fb0f4ad273307aeae
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-06-22 19:13:19 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const routers = require("/routers");
+const PORT = 3000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(routers);
+
+app.listen(PORT, () => {
+  console.log(`listening to PORT ${PORT}`);
+});

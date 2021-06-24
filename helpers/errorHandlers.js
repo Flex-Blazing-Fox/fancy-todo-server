4cb980200c4a905f257bbfe546c9fca269820f3d
juandari <juandaari70@gmail.com>
2021-06-24 20:43:22 +0700

    add errerHandler helper and edit documentation

---
+++ helpers/errorHandlers.js
@@ -0,0 +1,42 @@
+const errorHandlers = (err, req, res, next) => {
+  let statusCode, message
+
+  switch (err.name) {
+    case 'UniqueEmailError':
+      statusCode = 400
+      message = 'email already exists'
+      break
+    case 'IncorrectCredentialsError':
+      statusCode = 400
+      message = 'email or password is wrong'
+      break
+    case 'SequelizeValidationError':
+      statusCode = 400
+      message = 'Sequelize validation error'
+      break
+    case 'AuthenticationError':
+      statusCode = 401
+      message = 'invalid access token, put your access token in headers, named token'
+      break
+    case 'MissingAccessTokenError':
+      statusCode = 401
+      message = 'Please provide access token'
+      break
+    case 'ForbiddenError':
+      statusCode = 403
+      message = 'FORBIDDEN'
+      break
+    case 'NotFoundError':
+      statusCode = 404
+      message = 'todo not found'
+      break
+    default:
+      statusCode = 500
+      message = 'Internal server error'
+      break
+  }
+
+  res.status(statusCode).json({ message })
+}
+
+module.exports = errorHandlers

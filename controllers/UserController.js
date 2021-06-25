bad567fad6e586528ff43c0caba8b148bc83a76c
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-06-25 22:04:39 +0700

    Controller Login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const { User } = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class UserController {
     static register(req, res) {
@@ -14,6 +16,25 @@ class UserController {
             .status(err.status || 500)
             .json({ success: false, error: err.message || err})
         })
+    }
+
+    static login(req, res) {
+        const { email, password } = req.body
+
+        User.findOne({
+            where: {email}
+        })
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({success:true, message:"Login Success"})
+            } else {
+                throw {
+                    code: 401,
+                    message: "Email or Password npt found"
+                }
+            }
+        })
+        
     }
 }
 

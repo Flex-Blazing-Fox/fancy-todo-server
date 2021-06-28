e6d6b1ccdc046357eae455de4330cae20ddfdb2e
UmamKhoerul <umam.itdev@gmail.com>
2021-06-28 12:23:02 +0700

    revisi sedikit

--- routers/todoRoute.js
+++ routers/todoRoute.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 router.get('/',todo.listTodo)
 router.get('/:id',author,todo.listTodo_Id)
 router.post('/',todo.addTodo)
-router.get('/:id',author,todo.deleteTodo)
+router.delete('/:id',author,todo.deleteTodo)
 router.put('/:id',author,todo.putTodo)
 router.patch('/:id',author,todo.patchTodo)
 
